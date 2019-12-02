65bda7249acde04db4d9fd7e92fb94871342460f
wangqiang <wqexpore@163.com>
2019-12-02 11:40:01 +0800

    修改父pom文件打包

--- hello-springcloud-alibaba-nacos-consumer/src/test/java/com/jacken/hellospringcloudalibabanacosconsumer/HelloSpringcloudAlibabaNacosConsumerApplicationTests.java
+++ hello-springcloud-alibaba-nacos-consumer/src/test/java/com/jacken/hellospringcloudalibabanacosconsumer/HelloSpringcloudAlibabaNacosConsumerApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HelloSpringcloudAlibabaNacosConsumerApplicationTests {
+public class HelloSpringcloudAlibabaNacosConsumerApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
