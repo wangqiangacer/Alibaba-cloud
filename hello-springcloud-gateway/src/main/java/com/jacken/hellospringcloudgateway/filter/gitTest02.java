1e51d80dd209de499a463c37adb44a86d2efa07e
wangqiang <wqexpore@163.com>
2019-12-03 15:35:09 +0800

    修改父pom文件打包 测试git文件提交333

--- hello-springcloud-gateway/src/main/java/com/jacken/hellospringcloudgateway/filter/gitTest02.java
+++ hello-springcloud-gateway/src/main/java/com/jacken/hellospringcloudgateway/filter/gitTest02.java
@@ -1,4 +1,8 @@
 package com.jacken.hellospringcloudgateway.filter;
 
 public class gitTest02 {
+
+    public static void main(String[] args) {
+        System.out.println("1111111111111");
+    }
 }
