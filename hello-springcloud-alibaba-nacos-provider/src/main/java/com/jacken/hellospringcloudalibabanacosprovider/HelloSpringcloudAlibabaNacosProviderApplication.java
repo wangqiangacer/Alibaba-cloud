9a8c29d3f24288b89d174e8d077137b9145c1bab
王强 <wqexpore@163.com>
2019-12-07 22:13:15 +0800

    nacos 使用分布式配置中心nacos-config  实时刷新配置文件信息

--- hello-springcloud-alibaba-nacos-provider/src/main/java/com/jacken/hellospringcloudalibabanacosprovider/HelloSpringcloudAlibabaNacosProviderApplication.java
+++ hello-springcloud-alibaba-nacos-provider/src/main/java/com/jacken/hellospringcloudalibabanacosprovider/HelloSpringcloudAlibabaNacosProviderApplication.java
@@ -4,6 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+/**
+ *
+ * 注意:使用分布式配置中心 nacos config 加载配置文件
+ * 全局配置文件 加载顺序
+ * 1.bootstrap.properties
+ * 2.bootstarp.yml
+ * 3.application.properties
+ * 4.application.yml
+ *
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class HelloSpringcloudAlibabaNacosProviderApplication {
--- hello-springcloud-alibaba-nacos-provider/src/main/java/com/jacken/hellospringcloudalibabanacosprovider/controller/NacosProviderController.java
+++ hello-springcloud-alibaba-nacos-provider/src/main/java/com/jacken/hellospringcloudalibabanacosprovider/controller/NacosProviderController.java
@@ -1,18 +1,40 @@
 package com.jacken.hellospringcloudalibabanacosprovider.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 分布式配置中心  实时刷新配置文件信息
+ * ConfigurableApplicationContext  从全局配置环境中获取
+ *
+ * nacos 的多环境配置 bootstrp-dev.properites
+ * bootstrp-prod.properties
+ */
 @RestController
 public class NacosProviderController {
 
     @Value("${server.port}")
     private  String port;
 
+//    @Value("${name}")
+//    private  String name;
+//    @Value("${user.name}")
+//    private  String username;
+
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
+
+
     @RequestMapping("/echo/{message}")
     public  String echo(@PathVariable(value = "message") String message){
-    return "HELLO NACOS"+message+"i am from "+port;
+        String name = applicationContext.getEnvironment().getProperty("name");
+        String username = applicationContext.getEnvironment().getProperty("user.name");
+
+        return "HELLO NACOS"+message+"i am from "+port+"name:"+name+"username:"+username;
     }
 }
